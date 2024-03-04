69ad8b48d94cf285597652a504d13c17862a7874
Shyam Anand <shyam@Shyams-Pyyne-Macbook.local>
2024-03-04 11:26:30 +0100

    Added sql101

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,66 +1,9 @@
-resource "google_compute_network" "nw1" {
-  name = "mysql-server-vpc"
+module "mysql-server-vpc" {
+  source = "./network"
+
+  vpc-name = "mysql-server-vpc"
 }
 
-resource "google_compute_instance" "mysql-server" {
-  boot_disk {
-    auto_delete = true
-    device_name = "mysql-server"
-
-    initialize_params {
-      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240213"
-      size  = 10
-      type  = "pd-balanced"
-    }
-
-    mode = "READ_WRITE"
-  }
-
-  can_ip_forward      = false
-  deletion_protection = false
-  enable_display      = false
-  hostname            = "mysql-server.sql101.pyyne"
-
-  labels = {
-    goog-ec-src = "vm_add-tf"
-  }
-
-  machine_type = "e2-medium"
-  name         = "mysql-server"
-
-  network_interface {
-    access_config {
-      network_tier = "PREMIUM"
-    }
-
-    queue_count = 0
-    stack_type  = "IPV4_ONLY"
-    subnetwork  = "projects/lendo-413815/regions/northamerica-northeast1/subnetworks/default"
-  }
-
-  scheduling {
-    automatic_restart   = true
-    on_host_maintenance = "MIGRATE"
-    preemptible         = false
-    provisioning_model  = "STANDARD"
-  }
-
-  service_account {
-    email  = "153786853364-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
-  }
-
-  shielded_instance_config {
-    enable_integrity_monitoring = true
-    enable_secure_boot          = false
-    enable_vtpm                 = true
-  }
-
-  tags = ["http-server", "https-server"]
-  zone = "northamerica-northeast1-a"
-}
-
-resource "google_compute_firewall" "ssh-rule" {
-  name = "mysql-server-ssh"
-  network = google_compute_network.nw1.name
+module "mysql-instance" {
+  source = "./sql"
 }
--- gcp/provider.tf
+++ gcp/provider.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.51.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
     }
   }
 }
 
+variable "gcp_credentials_file" {
+  type    = string
+  default = ".credentials/sql101-415814-8a408cb1d321.json"
+}
+
 provider "google" {
-  credentials = file("sql101-415814-8a408cb1d321.json")
+  credentials = file(var.gcp_credentials_file)
 
   project = "sql101-415814"
   region  = "us-central1"
